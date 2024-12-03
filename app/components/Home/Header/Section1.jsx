0697dc4142b15967aa6e8fbc3165466033847df5
Vishwam Pandey <vishwampandey786@gmail.com>
2024-12-04 02:25:35 +0530

    111
    111

---
+++ app/components/Home/Header/Section1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Section1 = () => {
+  return (
+    <>
+      <div className="-mt-2 w-full h-[200px]  bg-gradient-to-t from-[#09090B] via-[#09090B]/70 to-transparent ">
+
+      
+</div>
+
+
+    </>
+  );
+};
+
+export default Section1;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -11,7 +14,25 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+
+
+      screens: {
+        'sm': '448px',
+        'md': '728px',
+        'lg': '1200px',
+      },
+
+
+      variants: {
+        extend: {
+          display: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
+        },
+      },
     },
   },
+
+
+ 
   plugins: [],
 };
