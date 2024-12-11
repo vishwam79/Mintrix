3614895aeacaed638841e86497ad2f93b12f8df1
Vishwam Pandey <vishwampandey786@gmail.com>
2024-12-12 01:25:57 +0530

    some css

--- src/components/Home/BentoGrid/Bento.jsx
+++ src/components/Home/BentoGrid/Bento.jsx
@@ -98,16 +98,16 @@ const Index = () => {
             </div>
           </div>
 
-          <div className="flex sm:w-full mt-8 lg:max-w-[340px]  h-[396px] pt-[32px] pr-[32px] pb-[32px] pl-[32px] flex-col justify-between items-start flex-nowrap bg-[#31316433]  bg-no-repeat rounded-[24px]  z-[27]">
+          <div className="flex w-auto mt-8   h-[396px] pt-[32px] pr-[32px] pb-[32px] pl-[32px] flex-col justify-between items-start flex-nowrap bg-[#31316433]  bg-no-repeat rounded-[24px]  z-[27]">
             <div className="flex w-auto flex-col gap-[20px] items-start self-stretch shrink-0 flex-nowrap relative z-[28]">
-              <span className="flex w-full justify-start items-start self-stretch shrink-0 font-['Plus_Jakarta_Sans'] text-[32px] font-semibold leading-[20px] tracking-[-0.32px] relative text-left z-30">
+              <span className="flex w-full justify-start items-start self-stretch shrink-0 font-['Plus_Jakarta_Sans'] text-[26px] font-semibold leading-[20px] tracking-[-0.32px] relative text-left z-30">
                 Prompt templates
               </span>
 
               <span>Use pre-made templates to jumpstart creativity.</span>
             </div>
 
-            <div className="w-auto h-[261.755px] shrink-0 relative overflow-hidden z-[3]  rounded-lg p-4">
+            <div className="w-full h-[261.755px] shrink-0 relative overflow-hidden z-[3]  rounded-lg p-4">
               <div className="flex flex-col items-center justify-center gap-4">
                 <div className="w-[120px] h-[35px] flex items-center justify-center bg-[rgba(98,106,181,0.21)] rounded-md border-[2px] border-violet-600  -rotate-45 mt-8 -right-3">
                   <span className="font-['Plus_Jakarta_Sans'] text-[16px] font-medium text-[#e9e5ff] ">
@@ -118,10 +118,10 @@ const Index = () => {
                 <img
                   src="/assets/images/camera_icon.png"
                   alt=""
-                  className="w-12 h-12 bg-[rgba(255,255,255,0.1)] p-1 rounded-full -ml-56"
+                  className="w-12 h-12 bg-[rgba(255,255,255,0.1)] p-1 rounded-full -ml-36"
                 />
 
-                <div className="flex min-w-fit px-2 gap-2 h-10 justify-center rounded-xl bg-[rgba(98,106,181,0.18)] rotate-90 -mt-16 ml-40">
+                <div className="flex min-w-fit px-2 gap-2 h-10 justify-center rounded-xl bg-[rgba(98,106,181,0.18)] rotate-90 -mt-16 ml-[100%]">
                   <button className="flex  items-center gap-1 justify-center  rounded-md  text-[#ddd6ff] uppercase text-[14px] font-base">
                     <span className="w-3 h-3 bg-pink-600 rounded-full"></span>
                     PNG
